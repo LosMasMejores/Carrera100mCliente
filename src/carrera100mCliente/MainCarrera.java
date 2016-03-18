f4c5738bbf0e510e1d90cde5ec977ed3ca50215c
jpmarcos <jpmarcos86@outlook.com>
2016-03-18 17:13:33 +0100

    Little fix

--- src/carrera100mCliente/MainCarrera.java
+++ src/carrera100mCliente/MainCarrera.java
@@ -15,10 +15,7 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriBuilder;
 
-public class MainCarrera {
-
-	static int NUM_ATLETAS = 2; // Numero de atletas por MainCarrera
-	
+public class MainCarrera {	
 	
 	// Funcion sacada de stackoverflow para ordenar mapas por el valor
 	public static <K, V extends Comparable<? super V>> Map<K, V> 
@@ -36,10 +33,29 @@ public class MainCarrera {
 	
 	public static void main(String[] args) {
 		
-		Semaphore sem_salida = new Semaphore(NUM_ATLETAS);
-		Semaphore sem_llegada = new Semaphore(NUM_ATLETAS);
+		Semaphore sem_salida;
+		Semaphore sem_llegada;
 		ArrayList<Atleta> atletas = new ArrayList<>();
 		int dorsal;
+		
+		int num_carreras = 1;
+		int num_atletas = 4;
+		
+		if (args.length == 0) {
+			System.out.println("USO: java mainCarrera [carreras] [atletas]");
+			System.out.println("\t[carreras] = numero de carreras participantes");
+			System.out.println("\t[atletas] = atletas por carrera");
+			return;
+		} else if (args.length > 2) {
+			System.out.println("ERROR: numero de parametros incorrectos");
+			return;
+		} else if (Integer.parseInt(args[0]) <= 0 || Integer.parseInt(args[1]) <= 0) {
+			System.out.println("ERROR: valor de los parametros <= 0");
+			return;
+		} else {
+			num_carreras = Integer.parseInt(args[0]);
+			num_atletas = Integer.parseInt(args[1]);
+		}
 		
 		Client client = ClientBuilder.newClient();
 		URI uri = UriBuilder.fromUri("http://localhost:8080/Carrera100mServidor").build();
@@ -47,29 +63,36 @@ public class MainCarrera {
 		
 		String respuesta = (target.path("carrera100")
 				.path("reinicio")
+				.queryParam("carreras", "" + num_carreras)
+				.queryParam("atletas", "" + num_atletas)
 				.request(MediaType.TEXT_PLAIN)
 				.get(String.class)); // Llamar al servicio /reinicio
 		
-		if (respuesta.contentEquals("COMPLETO"))
+		if (respuesta.contentEquals("COMPLETO") || respuesta.contentEquals("INCORRECTO")) {
+			System.out.println("ERROR: " + respuesta);
 			return;
-		else
+		} else {
 			dorsal = Integer.parseInt(respuesta); // Determinar que dorsales repartimos con la respuesta
-			
-		for (int i = 0; i < NUM_ATLETAS; i++) {
+		}
+		
+		sem_salida = new Semaphore(num_atletas);
+		sem_llegada = new Semaphore(num_atletas);
+		
+		for (int i = 0; i < num_atletas; i++) {
 			atletas.add(new Atleta(dorsal-i, sem_salida, sem_llegada));
 		}
 		
 		try {
 			
-			sem_llegada.acquire(NUM_ATLETAS); // Iniciamos los semaforos para coordinar los corredores
-			sem_salida.acquire(NUM_ATLETAS);  //
+			sem_llegada.acquire(num_atletas); // Iniciamos los semaforos para coordinar los corredores
+			sem_salida.acquire(num_atletas);  //
 			
-			for (int i = 0; i < NUM_ATLETAS; i++) {
+			for (int i = 0; i < num_atletas; i++) {
 				atletas.get(i).start();
 			}
 			
-			sem_salida.release(NUM_ATLETAS); // Iniciamos la carrera
-			sem_llegada.acquire(NUM_ATLETAS); // Esperamos a que terminen los corredores
+			sem_salida.release(num_atletas); // Iniciamos la carrera
+			sem_llegada.acquire(num_atletas); // Esperamos a que terminen los corredores
 			
 			Resultado resultado = target.path("carrera100")
 					.path("resultados")
